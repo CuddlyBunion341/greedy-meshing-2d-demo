19e6c981d058b3fea548caf9e707a9117c6af542
Daniel Bengl <daniel.bengl@renuo.ch>
2023-12-28 09:31:45 +0100

    Add method declarations

--- src/alg.ts
+++ src/alg.ts
@@ -6,23 +6,43 @@ export class GreedyMesher {
   private processed: DataMatrix<boolean>
   private areas: Area[]
   private currentArea: GrowableArea
+  private done: boolean
 
   constructor(blockData: DataMatrix<number>) {
     this.blockData = blockData
     this.processed = new DataMatrix(blockData.width, blockData.height, false)
+    this.done = false
+  }
+
+  call() {
+    while (!this.done) {
+      this.step()
+    }
   }
 
   step() {
+    if (this.done) return
+
     if (this.currentArea.canGrowX) {
-      // attempt to grow in x direction
+      this.attemptGrowX()
     } else if (this.currentArea.canGrowY) {
-      // attempt to grow in y direction
+      this.attemptGrowY()
     } else {
-      // store current area in areas array
-      // update processed matrix
-      // search for new area position
-      // if new position: create new area
-      // else: generate triangles
+      this.areas.push(this.currentArea)
+      this.updateProcessed()
+      const newArea = this.getNextArea()
+      if (newArea) {
+        this.currentArea = newArea
+      } else {
+        this.generateTriangles()
+        this.done = true
+      }
     }
   }
+
+  private attemptGrowX() { }
+  private attemptGrowY() { }
+  private updateProcessed() { }
+  private getNextArea(): GrowableArea | null { return null }
+  private generateTriangles() { }
 }
