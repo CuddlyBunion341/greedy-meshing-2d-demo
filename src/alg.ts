86f5a97801fe56f75678d3c6ec68ada326694d01
Daniel Bengl <daniel.bengl@renuo.ch>
2023-12-28 09:24:19 +0100

    Add pseudocode

--- src/alg.ts
+++ src/alg.ts
@@ -1,11 +1,28 @@
 import { DataMatrix } from "./data";
+import { Area, GrowableArea } from "./types";
 
 export class GreedyMesher {
   private blockData: DataMatrix<number>
   private processed: DataMatrix<boolean>
+  private areas: Area[]
+  private currentArea: GrowableArea
 
   constructor(blockData: DataMatrix<number>) {
     this.blockData = blockData
     this.processed = new DataMatrix(blockData.width, blockData.height, false)
+  }
+
+  step() {
+    if (this.currentArea.canGrowX) {
+      // attempt to grow in x direction
+    } else if (this.currentArea.canGrowY) {
+      // attempt to grow in y direction
+    } else {
+      // store current area in areas array
+      // update processed matrix
+      // search for new area position
+      // if new position: create new area
+      // else: generate triangles
+    }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export type Area = {
+  blockId: number
   x: number
   y: number
   w: number
